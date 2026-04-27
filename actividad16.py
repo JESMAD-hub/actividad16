2f77687d745d0d1c54fdb365014744847649798f
JESMAD-hub <jesusmadrigal@gmail.com>
2026-04-26 20:52:20 -0600

    Update actividad16.py

--- actividad16.py
+++ actividad16.py
@@ -11,7 +11,6 @@ from sklearn.naive_bayes import MultinomialNB
 from sklearn.svm import LinearSVC
 from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
 
-!pip install gensim
 # Word2Vec
 from gensim.models import Word2Vec
 
